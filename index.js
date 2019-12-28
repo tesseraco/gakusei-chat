3ac2506f22de2b528a2863869592b3190da2e443
Sherin Binu <sherinbinu@hotmail.com>
2019-12-29 01:27:29 +0530

    remove callback

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ server.listen(process.env.PORT || 3000);
 
 io.on('connection', function(socket){
 
-  socket.on('join', (params, callback) =>{
+  socket.on('join', (params) =>{
 
     try {
       const { userId, roomId } = params;
@@ -42,13 +42,12 @@ io.on('connection', function(socket){
       socket.join(roomId);
       io.to(roomId).emit('updateUsersList', room.users)
       socket.emit('alertMessage',`${userId} has joined the chat`);
-      callback();
       } catch(e) {
-        callback(e);
+        socket.emit('applicationError', e);
       }
   })
 
-  socket.on('createMessage', (params, callback) => {
+  socket.on('createMessage', (params) => {
     try {
       const { userId, roomId, message } = params;
 
@@ -56,32 +55,32 @@ io.on('connection', function(socket){
 
       let room = rooms.find(room => room.roomId === roomId);
       if(!room) {
-        return callback('Room does not exist')
+        return socket.emit('applicationError', 'Room does not exist')
       }
 
       let user = room.users.find(user => user.userId === userId);
       if(!user) {
-        return callback('Given user does not exist in the room')
+        return socket.emit('applicationError', 'Given user does not exist in the room')
       }
 
       io.to(roomId).emit('newMessage', generateMessage(userId, message));
     } catch(e) {
-      callback(e);
+      socket.emit('applicationError', e);
     }
   })
   
-  socket.on('typing', (params, callback) => {
+  socket.on('typing', (params) => {
     try {
       const {userId, roomId, status } = params;
 
       let room = rooms.find(room => room.roomId === roomId);
       if(!room) {
-        return callback('Room does not exist')
+        return socket.emit('applicationError', 'Room does not exist')
       }
 
       let user = room.users.find(user => user.userId === userId);
       if(!user) {
-        return callback('Given user does not exist in the room')
+        return socket.emit('applicationError', 'Given user does not exist in the room')
       }
 
       socket.broadcast.to(roomId).emit('updateTypingStatus', {
@@ -90,29 +89,29 @@ io.on('connection', function(socket){
       })
 
     } catch(e) {
-      callback(e);
+      socket.emit('applicationError', e);
     }
   })
 
-  socket.on('leave', (params, callback) => {
+  socket.on('leave', (params) => {
     try {
       const { userId, roomId } = params;
 
       let room = rooms.find(room => room.roomId === roomId);
       if(!room) {
-        return callback('Room does not exist')
+        return socket.emit('applicationError', 'Room does not exist')
       }
 
       let user = room.users.find(user => user.userId === userId);
       if(!user) {
-        return callback('Given user does not exist in the room')
+        return socket.emit('applicationError', 'Given user does not exist in the room')
       }
 
       room.users = rooms.users.filter(user => user.userId !== userId);
       socket.broadcast.to(roomId),emit('updateUsersList', rooms.users);
       socket.broadcast.to(roomId).emit('alertMessage', `${userId} has left the chat`);
     } catch(e) {
-      callback(e);
+      socket.emit('applicationError', e);
     }
   });
 });
